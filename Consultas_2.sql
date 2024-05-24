d4d48c1044d0731b722742c1202f8bd77da87e24
MaduhSamp <madusampietro@gmail.com>
2024-05-24 17:28:05 -0300

    Conclusão da 2° Lista de Consultas e 70% da  Modelagem 3

--- Consultas_2.sql
+++ Consultas_2.sql
@@ -27,3 +27,31 @@ FROM cliente c
 JOIN clienteJuridico cj ON c.codCliente = cj.codCliente;
 
 5 - Listar o nome fantasia, endereço, telefone, nome da cidade, sigla do estado de todos os clientes jurídicos que foram cadastrados entre 01/01/1999 e 30/06/2003.
+
+SELECT cj.nomeFantasia, c.endereco, c.telefone, cidade.nome, e.siglaEstado, c.dataCadastro
+FROM clienteJuridico cj
+JOIN cliente c ON cj.codCliente = c.codCliente
+JOIN cidade ON c.codCidade = cidade.codCidade
+JOIN estado e ON cidade.siglaEstado = e.siglaEstado
+WHERE c.dataCadastro > 01/01/1999 AND c.dataCadastro < 30/06/2003;
+
+6 - Listar o nome dos vendedores que realizaram vendas para o cliente pessoa jurídica com nome fantasia ‘Gelinski’.
+
+SELECT venda.codVenda, v.nome, cj.nomeFantasia
+FROM venda
+JOIN vendedor v ON venda.codVendedor = v.codVendedor
+JOIN clienteJuridico cj ON venda.codCliente = cj.codCliente
+WHERE cj.nomeFantasia = 'Gelinski';
+
+7 - Listar o código, o número do lote e o nome dos produtos que estão com a data de validade vencida.
+
+SELECT pl.codProduto, pl.numeroLote, p.descricao, pl.dataValidade
+FROM produtoLote pl
+JOIN produto p ON pl.codProduto = p.codProduto
+WHERE pl.dataValidade < CURRENT_TIMESTAMP;
+
+8 - Listar o nome dos departamentos e o nome dos vendedores neles lotados.
+
+SELECT d.nome, v.nome
+FROM vendedor v
+JOIN departamento d ON v.codDepartamento = d.codDepartamento;
---
+++ Modelagem_3.sql
@@ -0,0 +1,100 @@
+CREATE TABLE departamento (
+	codDepartamento INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40) UNIQUE NOT NULL,
+    descricaoFuncional VARCHAR(80),
+    localizacao MEDIUMTEXT
+);
+
+CREATE TABLE estado (
+	siglaEstado CHAR(2) PRIMARY KEY,
+    nome VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE cidade (
+	codCidade INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) UNIQUE NOT NULL,
+    siglaEstado CHAR(2) NOT NULL REFERENCES estado(siglaEstado) ON DELETE no action ON UPDATE cascade
+);
+
+CREATE TABLE vendedor (
+	codVendedor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(60) NOT NULL,
+    dataNascimento DATE,
+    endereco VARCHAR(60),
+    cep CHAR(8),
+    telefone VARCHAR(20),
+    codCidade INT DEFAULT 1,
+    dataContratacao DATE DEFAULT(CURDATE()),
+    codDepartamento INT,
+    FOREIGN KEY (codCidade) REFERENCES cidade (codCidade) ON DELETE cascade ON UPDATE cascade,
+    FOREIGN KEY (codDepartamento) REFERENCES departamento (codDepartamento) ON DELETE set null ON UPDATE cascade
+);
+
+CREATE TABLE cliente (
+	codCliente INT AUTO_INCREMENT PRIMARY KEY,
+    endereco VARCHAR(60),
+    codCidade INT NOT NULL,
+    telefone VARCHAR(20),
+    tipo CHAR(1) CHECK (tipo IN ('F', 'J')),
+    dataCadastro DATE DEFAULT(CURDATE()),
+    cep CHAR(8),
+    CONSTRAINT fk_cliente_cidade FOREIGN KEY (codCidade) REFERENCES cidade (codCidade) ON DELETE no action ON UPDATE cascade
+);
+
+CREATE TABLE clienteFisico (
+	codCliente INT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    dataNascimento DATE,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    rg VARCHAR(8),
+    FOREIGN KEY (codCliente) REFERENCES cliente (codCliente) ON DELETE restrict ON UPDATE cascade
+);
+
+CREATE TABLE clienteJuridico (
+	codCliente INT PRIMARY KEY,
+    nomeFantasia VARCHAR(80) UNIQUE,
+    razaoSocial VARCHAR(80) UNIQUE NOT NULL,
+    ie VARCHAR(20) UNIQUE NOT NULL,
+    cgc VARCHAR(20) UNIQUE NOT NULL,
+    FOREIGN KEY (codCliente) REFERENCES cliente (codCliente) ON DELETE restrict ON UPDATE cascade
+);
+
+CREATE TABLE classe (
+	codClasse INT AUTO_INCREMENT PRIMARY KEY,
+    sigla VARCHAR(12),
+    nome VARCHAR(40) NOT NULL,
+    descricao VARCHAR(80)
+);
+
+CREATE TABLE produto (
+	codProduto INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(40) NOT NULL,
+    unidadeMedida CHAR(2),
+    embalagem VARCHAR(15) DEFAULT 'Fardo',
+    codClasse INT,
+    precoVenda DECIMAL(14, 2),
+    estoqueMinimo DECIMAL(14, 2),
+    FOREIGN KEY (codClasse) REFERENCES classe (codClasse) ON DELETE set null ON UPDATE set null
+);
+
+CREATE TABLE produtoLote (
+    codProduto INT, 
+    numeroLote INT, 
+    quantidadeAdquirida DECIMAL(14, 2), 
+    quantidadeVendida DECIMAL(14, 2), 
+    precoCusto DECIMAL(14, 2), 
+    dataValidade DATE, 
+    PRIMARY KEY (codProduto, numeroLote), 
+    FOREIGN KEY (codProduto) REFERENCES produto (codProduto) ON DELETE cascade ON UPDATE cascade
+);
+
+CREATE TABLE venda (
+	codVenda INT PRIMARY KEY,
+    codCliente INT,
+    codVendedor INT DEFAULT 100,
+    dataVenda DATE DEFAULT(CURDATE()),
+    enderecoEntrega VARCHAR(60),
+    status VARCHAR(30),
+    FOREIGN KEY (codCliente) REFERENCES cliente (codCliente) ON DELETE cascade ON UPDATE no action,
+    FOREIGN KEY (codVendedor) REFERENCES vendedor (codVendedor) ON DELETE set default ON UPDATE cascade
+);
